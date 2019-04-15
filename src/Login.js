e4780721471507fb737d496d92cf2d18ad795f12
rishiguptarv <rishiguptarv@gmail.com>
2019-04-15 18:52:02 +0530

    1 file modified

--- src/Login.js
+++ src/Login.js
@@ -2,7 +2,8 @@ import React, { Component } from 'react';
 import { render } from 'react-dom';
 import '../style.css';
 import Button from '@material-ui/core/Button';
-import PhoneInput from 'react-phone-number-input'
+import PhoneInput from 'react-phone-number-input';
+import Select from '@material-ui/core/Select';
 
 class Login extends Component {
   constructor() {
@@ -19,7 +20,16 @@ class Login extends Component {
     placeholder="Enter phone number"
     value={ this.state.phone }
     onChange={ phone => this.setState({ phone }) } />
+     <Select
+            value={this.state.age}
+            onChange={this.handleChange}
+            inputProps={{
+              name: 'age',
+              id: 'age-simple',
+            }}
+          />
     </div>
+    
     );
   }
 }
