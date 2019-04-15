dc03c0d61c5b5049858df7844d0ab2293b78d0ff
rishiguptarv <rishiguptarv@gmail.com>
2019-04-15 20:19:20 +0530

    1 file modified

--- src/Login.js
+++ src/Login.js
@@ -2,6 +2,7 @@ import React, { Component } from 'react';
 import { render } from 'react-dom';
 import '../style.css';
 import Button from '@material-ui/core/Button';
+import 'react-phone-number-input/style.css'
 import PhoneInput from 'react-phone-number-input';
 import Select from '@material-ui/core/Select';
 import Grid from '@material-ui/core/Grid';
@@ -19,13 +20,12 @@ class Login extends Component {
     <div className="Login-component">
     <Grid
   container
-  direction="row"
+  direction="column"
   justify="center"
   alignItems="center"> 
     <PhoneInput
-    placeholder="Enter phone number"
-    value={ this.state.phone }
-    onChange={ phone => this.setState({ phone }) } />  
+  value={ this.state.value }
+  onChange={ value => this.setState({ value }) }/>
     </Grid>
     </div>
   
