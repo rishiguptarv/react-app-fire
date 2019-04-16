03f96d7cdfaa4dece5c310de50c3e57ace61abc0
rishiguptarv <rishiguptarv@gmail.com>
2019-04-16 13:12:20 +0530

    1 file modified

--- src/Login.js
+++ src/Login.js
@@ -2,7 +2,6 @@ import React, { Component } from 'react';
 import { render } from 'react-dom';
 import '../style.css';
 import Button from '@material-ui/core/Button';
-import 'react-phone-number-input/style.css'
 import Select from '@material-ui/core/Select';
 import Grid from '@material-ui/core/Grid';
 
