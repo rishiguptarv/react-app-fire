12896735718b656b329432c7c9d966bb413a2ff6
rishiguptarv <rishiguptarv@gmail.com>
2019-04-17 16:47:36 +0530

    2 files modified

--- src/Login.js
+++ src/Login.js
@@ -1,9 +1,19 @@
 import React from 'react';
 import { render } from 'react-dom';
 import PropTypes from 'prop-types';
-import faker from 'faker';
 import _ from 'lodash';
 import {Select, BootstrapInput, MenuItem, Input, Button } from '@material-ui/core';
+import firebase from firebase;
+
+  const config = {
+    apiKey: "AIzaSyB5OTFxrmsLItoxP6ISO9gxTtjcSN9FlXQ",
+    authDomain: "phone-login-536c9.firebaseapp.com",
+    databaseURL: "https://phone-login-536c9.firebaseio.com",
+    projectId: "phone-login-536c9",
+    storageBucket: "phone-login-536c9.appspot.com",
+    messagingSenderId: "305382960164"
+  };
+  firebase.initializeApp(config);
 
 
  class Login extends React.Component {
