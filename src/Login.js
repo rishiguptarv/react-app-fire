34c12be5bae85498f4a6fb49b3d740a27d8c581d
rishiguptarv <rishiguptarv@gmail.com>
2019-04-17 18:42:24 +0530

    2 files modified

--- src/Login.js
+++ src/Login.js
@@ -3,7 +3,8 @@ import { render } from 'react-dom';
 import PropTypes from 'prop-types';
 import _ from 'lodash';
 import {Select, BootstrapInput, MenuItem, Input, Button } from '@material-ui/core';
-import firebase from firebase;
+import firebase from 'firebase/app';
+import 'firebase/auth';
 
   const config = {
     apiKey: "AIzaSyB5OTFxrmsLItoxP6ISO9gxTtjcSN9FlXQ",
@@ -13,13 +14,26 @@ import firebase from firebase;
     storageBucket: "phone-login-536c9.appspot.com",
     messagingSenderId: "305382960164"
   };
-  firebase.initializeApp(config);
+  !firebase.apps.length ? firebase.initializeApp(config) : firebase.app();
+   //firebase.App.initializeApp(config);
 
 
  class Login extends React.Component {
-  //  constructor(props){
-  //    super(props);
-  //  }
+ componentDidMount(){
+   let window='';
+        firebase.auth().languageCode = 'india';
+        var phoneNumber ="+918923569047";
+        var appVerifier = window.recaptchaVerifier;
+        // window.recaptchaVerifier = new firebase.auth.RecaptchaVerifier('sign-in-button', {
+        //     'size': 'invisible',
+        //     'callback': function(response) {
+        //         // reCAPTCHA solved, allow signInWithPhoneNumber.
+        //         onSignInSubmit();
+        //     }
+        // });
+        window.recaptchaVerifier = new firebase.auth.RecaptchaVerifier('recaptcha-container');
+   
+ }
       state =  {
       login:'',
       countryCode:'',
@@ -33,6 +47,7 @@ import firebase from firebase;
 
   render(){
      console.log(this.state);
+     
      const { classes } = this.props;
     return (
       <div>
@@ -43,9 +58,10 @@ import firebase from firebase;
             </Select>
 
       <Input inputProps={{name: 'phone',id: 'phone'}} placeholder={"phone"} value= {this.state.phone} onChange={this.onTextChange} />
-       <Button variant="contained" color="primary">
+       <Button variant="contained" color="primary" id='sign-in-button'>
       Send Code
     </Button>
+    <div id="recaptcha-container"></div>
       </div>
       
     );
