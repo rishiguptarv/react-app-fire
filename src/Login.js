606738536162287d9ae255ce0dd220d2eb6a2db8
rishiguptarv <rishiguptarv@gmail.com>
2019-04-17 16:24:00 +0530

    1 file modified

--- src/Login.js
+++ src/Login.js
@@ -1,32 +1,43 @@
 import React from 'react';
+import { render } from 'react-dom';
+import PropTypes from 'prop-types';
 import faker from 'faker';
 import _ from 'lodash';
-import {Select, BootstrapInput, MenuItem, Input } from '@material-ui/core';
+import {Select, BootstrapInput, MenuItem, Input, Button } from '@material-ui/core';
 
 
  class Login extends React.Component {
+  //  constructor(props){
+  //    super(props);
+  //  }
+      state =  {
+      login:'',
+      countryCode:'',
+      phone:''
+      }
 
-  state =  {
-    login:"",
-    countryCode:''
-  }
-
-  onCodeChange(this){
-    this.setState= {"countryCode":this.value};
-   console.log(state);
+  onTextChange =(event)=>{
+    this.setState({ [event.target.name]: event.target.value });
+   console.log(event.state);
   }
 
   render(){
+     console.log(this.state);
+     const { classes } = this.props;
     return (
       <div>
-      <Select placeholder='Select your country' onChange={this.onCodeChange.bind(this)} value={this.state.countryCode}>
-            <MenuItem value={+91}>India</MenuItem>
-            <MenuItem value={+92}>dummy</MenuItem>
-            <MenuItem value={+93}>dummy</MenuItem>
+      <Select inputProps={{name: 'countryCode',id: 'countryCode'}}placeholder='Select your country' onChange={this.onTextChange} value={this.state.countryCode}>
+            <MenuItem value={'+91'}>+91</MenuItem>
+            <MenuItem value={'+92'}>+92</MenuItem>
+            <MenuItem value={'+93'}>+93</MenuItem>
             </Select>
 
-      <Input placeholder='phone' />
+      <Input inputProps={{name: 'phone',id: 'phone'}} placeholder={"phone"} value= {this.state.phone} onChange={this.onTextChange} />
+       <Button variant="contained" color="primary">
+      Send Code
+    </Button>
       </div>
+      
     );
   }
 
